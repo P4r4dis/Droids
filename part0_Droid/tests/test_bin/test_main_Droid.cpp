a037abe47923f4d884d4cc0aba764ae42f3ad18f
P4r4dis <adil.d.pro@gmail.com>
2023-01-27 02:48:07 +0100

    [ADD] - unit test Droid::Droid, Destructor, getters and setters add

--- part0_Droid/tests/test_bin/test_main_Droid.cpp
+++ part0_Droid/tests/test_bin/test_main_Droid.cpp
@@ -3,11 +3,97 @@
 #include <criterion/redirect.h>
 #include <criterion/logging.h>
 #include <criterion/parameterized.h>
+#include <signal.h>
 
 #include "../test_include/test_Droid.hpp"
 #include <iostream>
 
-Test(test, tests)
+Test(Droid, test_construction_whitout_parameter, .init = redirect_all_stdout)
 {
-    cr_assert(1 == 1);
+    Droid       droid;
+
+    cr_assert(droid.getId() == ""); //stdstring
+    cr_assert(droid.getEnergy() == 50); //size_t
+    cr_assert(droid.getAttack() == 25); //const size_t
+    cr_assert(droid.getToughness() == 15); //const size_t
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Standing by"); //std::string *
+
+    droid.setId("1");
+    cr_assert(droid.getId() == "1"); //stdstring
+
+    droid.setEnergy(100);
+    cr_assert(droid.getEnergy() == 100); //size_t
+
+    droid.setStatus(new std::string("Pending by"));
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Pending by"); //std::string *
+
+    cr_assert_stdout_eq_str("Droid '' Activated\n");
+}
+
+Test(Droid, test_construction_whith_parameter, .init = redirect_all_stdout)
+{
+    Droid       droid("Avenger");
+
+    cr_assert(droid.getId() == "Avenger");
+    cr_assert(droid.getEnergy() == 50);
+    cr_assert(droid.getAttack() == 25);
+    cr_assert(droid.getToughness() == 15);
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Standing by");
+
+    droid.setId("1");
+    cr_assert(droid.getId() == "1");
+
+    droid.setEnergy(100);
+    cr_assert(droid.getEnergy() == 100);
+
+    droid.setStatus(new std::string("Pending by"));
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Pending by");
+
+    cr_assert_stdout_eq_str("Droid 'Avenger' Activated\n");
+}
+
+Test(Droid, test_construction_copyConstructor, .init = redirect_all_stdout)
+{
+    Droid       droid;
+    Droid       copyDroid(droid);
+
+    cr_assert_neq(&droid, &copyDroid);
+
+    cr_assert(droid.getId() == ""); //stdstring
+    cr_assert(copyDroid.getId() == "");
+    cr_assert(droid.getEnergy() == 50); //size_t
+    cr_assert(copyDroid.getEnergy() == 50);
+    cr_assert(droid.getAttack() == 25); //const size_t
+    cr_assert(copyDroid.getAttack() == 25);
+    cr_assert(droid.getToughness() == 15); //const size_t
+    cr_assert(copyDroid.getToughness() == 15);
+
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Standing by"); //std::string *
+    cr_assert_str_eq(copyDroid.getStatus()->c_str(), "Standing by");
+
+    droid.setId("1");
+    copyDroid.setId("1");
+    cr_assert(droid.getId() == "1"); //stdstring
+    cr_assert(copyDroid.getId() == "1");
+
+    droid.setEnergy(100);
+    copyDroid.setEnergy(100);
+    cr_assert(droid.getEnergy() == 100); //size_t
+    cr_assert(copyDroid.getEnergy() == 100);
+
+    droid.setStatus(new std::string("Pending by"));
+    copyDroid.setStatus(new std::string("Pending by"));
+    cr_assert_str_eq(droid.getStatus()->c_str(), "Pending by"); //std::string *
+    cr_assert_str_eq(copyDroid.getStatus()->c_str(), "Pending by");
+
+    cr_assert_stdout_eq_str("Droid '' Activated\nDroid '' Activated, Memory Dumped\n");
+}
+
+Test(Droid, test_destruction, .init = redirect_all_stdout)
+{
+    Droid       droid;
+
+    droid.~Droid();
+    cr_assert(droid.getStatus() == nullptr);
+    cr_assert_stdout_eq_str("Droid '' Activated\nDroid '' Destroyed\n");
 }
