87f3bc5ab0f55d361fd4cee2872c646428dd375a
P4r4dis <adil.d.pro@gmail.com>
2023-01-27 19:07:42 +0100

    [ADD] - feature Droid::Droid construction / destruction, getters and setters add

--- part0_Droid/include/Droid.hpp
+++ part0_Droid/include/Droid.hpp
@@ -1,4 +1,38 @@
 #ifndef     __DROID__
 #   define  __DROID__
+#include <string>
+#include <memory>
+
+class   Droid
+{
+    private:
+        std::string         _Id;
+        size_t              _Energy;
+        const size_t        _Attack;
+        const size_t        _Toughness;
+        // std::string         *_Status;
+        std::unique_ptr<std::string> _Status;
+    public:
+        Droid(std::string Id = ""); //CUSTOM CTOR
+        Droid(const Droid &copyDroid); //COPY CTOR
+        ~Droid(void);
+
+        std::string         getId(void);
+        void                setId(std::string Id);
+
+        size_t              getEnergy(void);
+        void                setEnergy(size_t Energy);
+
+        size_t              getAttack(void) const;
+
+        size_t              getToughness(void) const;
+
+        std::string         *getStatus(void);
+        void                setStatus(std::string *Status);
+
+        Droid               &operator=(const Droid &rhs);
+
+
+};
 
 #endif //   !__DROID__
--- part0_Droid/src/Droid.cpp
+++ part0_Droid/src/Droid.cpp
@@ -1,1 +1,66 @@
 #include "../include/Droid.hpp"
+#include <iostream>
+
+Droid::Droid(std::string Id) :  _Id(Id),
+                                _Energy(50),
+                                _Attack(25),
+                                _Toughness(15),
+                                _Status(std::make_unique<std::string>("Standing by"))//_Status(new std::string("Standing by"))
+{
+    std::cout << "Droid '" << _Id << "' Activated" << std::endl;
+}
+
+Droid::Droid(const Droid &copyDroid) :  _Id(copyDroid._Id),
+                                        _Energy(copyDroid._Energy),
+                                        _Attack(copyDroid._Attack),
+                                        _Toughness(copyDroid._Toughness),
+                                        _Status(std::make_unique<std::string>(*copyDroid._Status))//_Status(new std::string(*copyDroid._Status))
+{
+    std::cout << "Droid '" << copyDroid._Id << "' Activated, Memory Dumped" << std::endl;
+}
+
+Droid::~Droid(void)
+{
+    if (_Status)
+        std::cout << "Droid '" << _Id << "' Destroyed" << std::endl;
+}
+
+std::string     Droid::getId(void)
+{
+    return _Id;
+}
+
+void            Droid::setId(std::string Id)
+{
+    _Id = Id;
+}
+
+size_t          Droid::getEnergy(void)
+{
+    return _Energy;
+}
+
+void            Droid::setEnergy(size_t Energy)
+{
+    _Energy = Energy;
+}
+
+size_t          Droid::getAttack(void) const
+{
+    return _Attack;
+}
+
+size_t          Droid::getToughness(void) const
+{
+    return _Toughness;
+}
+
+std::string     *Droid::getStatus(void)
+{
+        return _Status.get();
+}
+
+void            Droid::setStatus(std::string *Status)
+{
+    _Status = std::make_unique<std::string>(*Status);
+}
