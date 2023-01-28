360a0fb8d51f9800dcd736ba28d281eed585120b
P4r4dis <adil.d.pro@gmail.com>
2023-01-28 13:58:03 +0100

    [ADD] Droid main added to main project.

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,6 +1,28 @@
 #include "../part0_Droid/include/Droid.hpp"
+#include <iostream>
 
 int     main(void)
 {
+    std::cout << "PART 0 - Droid MAIN - START :" << std::endl;
+    Droid   d;
+    Droid   d1("Avenger");
+    size_t  Durasel = 200;
+
+    std::cout << d << std::endl;
+    std::cout << d1 << std::endl;
+    d = d1;
+    d.setStatus(new std::string("Kill Kill Kill!"));
+    d << Durasel;
+    std::cout << d << "--" << Durasel << std::endl;
+    Droid d2 = d;
+    d.setId("Rex");
+    std::cout << (d2 != d) << std::endl;
+
+    d2.~Droid();
+    d1.~Droid();
+    d.~Droid();
+    std::cout << "PART 0 - Droid MAIN - END :" << std::endl;
+
+
     return 0;
 }
