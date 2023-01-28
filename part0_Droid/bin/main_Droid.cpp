34875bda180844d4845c37e1cc723a28e3ab6c08
P4r4dis <adil.d.pro@gmail.com>
2023-01-28 13:51:20 +0100

    [ADD] - main function add

--- part0_Droid/bin/main_Droid.cpp
+++ part0_Droid/bin/main_Droid.cpp
@@ -57,6 +57,20 @@ int     main(void)
     copyDroid_param.~Droid();
     std::cout << std::endl;
 
-    // std::cout << "REAL DROID MAIN :" << std::endl;
+    std::cout << "REAL DROID MAIN :" << std::endl;
+    Droid   d;
+    Droid   d1("Avenger");
+    size_t  Durasel = 200;
+
+    std::cout << d << std::endl;
+    std::cout << d1 << std::endl;
+    d = d1;
+    d.setStatus(new std::string("Kill Kill Kill!"));
+    d << Durasel;
+    std::cout << d << "--" << Durasel << std::endl;
+    Droid d2 = d;
+    d.setId("Rex");
+    std::cout << (d2 != d) << std::endl;
+    
     return 0;
 }
