290537582b05190155a57554af3b40397dff4c19
P4r4dis <adil.d.pro@gmail.com>
2023-01-31 16:37:46 +0100

    [ADD] - DroidMemory main add

--- part1_DroidMemory/bin/main_DroidMemory.cpp
+++ part1_DroidMemory/bin/main_DroidMemory.cpp
@@ -101,6 +101,18 @@ int     main(void)
                 << std::endl;
     droid.~Droid();
     std::cout << "REAL DROIDMEMORY MAIN :" << std::endl;
+    DroidMemory     mem1;
+    mem1 += 42;
+
+    DroidMemory mem2 = mem1;
+    std::cout << mem1 << std::endl;
+
+    DroidMemory mem3;
+    mem3 << mem1;
+    mem3 >> mem1;
+    mem3 << mem1;
+    std::cout << mem3 << std::endl;
+    std::cout << mem1 << std::endl;
     
     return 0;
 }
