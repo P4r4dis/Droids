b51bbfea58d4254f6757f98cb96fc5e078e21aaa
P4r4dis <adil.d.pro@gmail.com>
2023-02-07 17:38:42 +0100

    [ADD] - code for main function add

--- part3_Carrier/bin/main_Carrier.cpp
+++ part3_Carrier/bin/main_Carrier.cpp
@@ -65,6 +65,37 @@ int     main(void)
     std::cout   << "carrier.getSpeed after Droids in  at board = "
                 << carrier.getSpeed() << std::endl;
 
+    Carrier     carrier2("HellExpress");
+
+    Droid *droid1 = new Droid("Commander");
+    Droid *droid2 = new Droid("Sergent");
+    Droid *droid3 = new Droid("Troufiont");
+    Droid *droid4 = new Droid("Groupie");
+    Droid *droid5 = new Droid("BeerHolder");
+
+    std::cout << "Overloading of << operator = ";
+    carrier2 << droid1 << droid2 << droid3 << droid4 << droid5;
+    std::cout <<carrier2.getSpeed() << droid1 << std::endl;
+    std::cout <<carrier2 << std::endl;
+    std::cout << "Overloading of >> operator = ";
+    carrier2 >> droid1 >> droid2 >> droid3;
+    std::cout <<carrier2.getSpeed() << std::endl;
+    std::cout <<carrier2 << std::endl;
+    std::cout << "Overloading of [] operator = ";
+    carrier2[0] = droid1;
+    std::cout <<carrier2.getSpeed() << std::endl;
+    std::cout <<carrier2 << std::endl;
+    std::cout << "Overloading of ~ operator = ";
+    std::cout << (~carrier2).getSpeed() << std::endl;
+    std::cout <<carrier2 << std::endl;
+    std::cout << "Overloading of () operator = ";
+    carrier2(4,2);
+    std::cout <<carrier2 << std::endl;
+    std::cout << "Carriercarrier2 after destruction = ";
+    carrier2.~Carrier();
+    std::cout <<carrier2 << std::endl;
+    std::cout << "REAL MAIN STARTING :--------------------------" << std::endl;
+
     Carrier     c("HellExpress");
 
     Droid *d1 = new Droid("Commander");
@@ -73,26 +104,20 @@ int     main(void)
     Droid *d4 = new Droid("Groupie");
     Droid *d5 = new Droid("BeerHolder");
 
-    std::cout << "Overloading of << operator = ";
     c << d1 << d2 << d3 << d4 << d5;
-    std::cout << c.getSpeed() << d1 << std::endl;
-    std::cout << c << std::endl;
-    std::cout << "Overloading of >> operator = ";
+    std::cout <<c.getSpeed() << d1 << std::endl;
     c >> d1 >> d2 >> d3;
-    std::cout << c.getSpeed() << std::endl;
-    std::cout << c << std::endl;
-    std::cout << "Overloading of [] operator = ";
+    std::cout <<c.getSpeed() << std::endl;
     c[0] = d1;
-    std::cout << c.getSpeed() << std::endl;
-    std::cout << c << std::endl;
-    std::cout << "Overloading of ~ operator = ";
     std::cout << (~c).getSpeed() << std::endl;
+    carrier2(4,2);
     std::cout << c << std::endl;
-    std::cout << "Overloading of () operator = ";
-    c(4,2);
+    c(-15, 4);
     std::cout << c << std::endl;
-    std::cout << "Carrier c after destruction = ";
-    c.~Carrier();
+    c[3] = 0;
+    c[4] = 0;
+    (~c)(-15, 4);
     std::cout << c << std::endl;
+    
     return 0;
 }
